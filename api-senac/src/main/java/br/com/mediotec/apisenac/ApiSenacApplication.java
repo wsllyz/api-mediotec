d6cd7c06c5a4dc9ef0077ff39266e32a6dffd732
Miguel <miguel.amaral.sales@gmail.com>
2024-08-20 20:21:32 -0300

    start

---
+++ api-senac/src/main/java/br/com/mediotec/apisenac/ApiSenacApplication.java
@@ -0,0 +1,13 @@
+package br.com.mediotec.apisenac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSenacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSenacApplication.class, args);
+    }
+
+}
---
+++ api-senac/src/test/java/br/com/mediotec/apisenac/ApiSenacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mediotec.apisenac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSenacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
